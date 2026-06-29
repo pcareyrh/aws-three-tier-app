0bfa67179062c2b47b55d14cd380d3a7fbc11fa9
Pat Carey <pcarey@hashicorp.com>
2026-06-29 20:33:56 +1000

    update policy for new name

--- terraform/01-infra/oidc-setup.tf
+++ terraform/01-infra/oidc-setup.tf
@@ -54,7 +54,7 @@ data "aws_iam_policy_document" "deploy" {
           "ecr:DescribeImages",
           "ecr:ListImages"
     ]
-    resources = ["arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/${var.name}-ecr-private"]
+    resources = ["arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/tasky-app"]
   }
 
   statement {
