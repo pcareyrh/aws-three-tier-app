5049a48e0e6c4003c5ec84fa400665a593e2ed69
Pat Carey <pcarey@hashicorp.com>
2026-06-30 11:07:09 +1000

    add random suffix

--- terraform/01-infra/cloudtrail.tf
+++ terraform/01-infra/cloudtrail.tf
@@ -17,8 +17,14 @@ resource "aws_cloudtrail" "cloudtrail" {
   }
 }
 
+resource "random_string" "cloudtrail_bucket_suffix" {
+  length  = 5
+  upper   = false
+  special = false
+}
+
 resource "aws_s3_bucket" "cloudtrail-bucket" {
-  bucket        = "${var.name}-cloudtrail-3tier-trail"
+  bucket        = "${var.name}-cloudtrail-3tier-trail-${random_string.cloudtrail_bucket_suffix.result}"
   force_destroy = true
 }
 
